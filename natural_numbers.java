b3626f449fc031a7a6093057eab26db5e908c286
SulemanAI <muhammadsuleman0759@gmail.com>
2024-10-23 09:39:40 +0500

    Add files via upload

---
+++ natural_numbers.java
@@ -0,0 +1,12 @@
+class HelloWorld {
+    public static void main(String[] args) {
+    // int i = 1;
+    int sum=0;
+    System.out.println("The sum of first 10 natural numbers");
+    for(int i = 1 ; i<=10 ;i++){
+        // sum += i;
+        sum  = sum + i;
+        System.out.println(sum);
+    }
+    }
+}

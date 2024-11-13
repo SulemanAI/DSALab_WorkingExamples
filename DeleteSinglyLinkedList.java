14ce87bc18cc51317cdfb159b3eac2751e5cf1ac
SulemanAI <muhammadsuleman0759@gmail.com>
2024-11-13 21:41:44 +0500

    Update DeleteSinglyLinkedList.java

--- DeleteSinglyLinkedList.java
+++ DeleteSinglyLinkedList.java
@@ -1,12 +1,61 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+class Node {
+    int data;
+    Node next;
 
-/**
- *
- * @author muham
- */
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    Node head;
+
+    public void deleteList() {
+        head = null;
+        System.out.println("List has been deleted.");
+    }
+
+    public void append(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public void printList() {
+        if (head == null) {
+            System.out.println("List is empty.");
+            return;
+        }
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+}
+
 public class DeleteSinglyLinkedList {
-    
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.append(10);
+        list.append(20);
+        list.append(30);
+        
+        System.out.println("Original List:");
+        list.printList();
+
+        list.deleteList();
+        
+                System.out.println("After deletion:");
+        list.printList();
+    }
 }

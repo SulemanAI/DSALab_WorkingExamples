68ee24ee33f4f6ac133c09c387c3769fd226af54
SulemanAI <muhammadsuleman0759@gmail.com>
2024-11-19 22:04:17 -0800

    Add files via upload

--- Linkedlist.java
+++ Linkedlist.java
@@ -1,76 +1,120 @@
-class Node{
-    int data=23;
-    Node address;
+package com.mycompany.mavenproject1;
 
+public class LinkedList { // Rename this class to LinkedList
+    private Node front;
+    private Node rear;
 
-public Node(int data){
-    this.data = data;
-    this.address = null;
-}
-}
+    static class Node {
+        public int data;
+        public Node next;
 
-public class LinkedList {
-    
-        Node head , tail;
-        int size;
-        
-    public LinkedList(){
-        head = tail = null;
-        size = 0 ;
-    }
-    
-    public void add (int d){
-        Node n = new Node(d);
-        
-        if(head == null){
-            head=tail=n;
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
         }
-        else{
-            tail.address = n;
-            tail = n;
+    }
+
+    public boolean isEmpty() {
+        return (front == null);
+    }
+
+    public void insertLast(int new_data) {  // Insert at end of queue
+        Node new_node = new Node(new_data);
+        if (isEmpty()) {  // If empty queue
+            front = new_node;  // Front --> new_node
+        } else {
+            rear.next = new_node;  // Old rear --> new_node
         }
-        size++ ;
+        rear = new_node;  // New_node <-- rear
     }
-    
-    public int getSize(){
-        return size;
+
+    public void insertFirst(int new_data) {  // Insert at start of queue
+        Node new_node = new Node(new_data);
+        if (isEmpty()) {  // If empty list
+            front = new_node;  // First --> new_node
+        } else {
+            new_node.next = front;  // New_node -> next --> front
+        }
+        front = new_node;  // Front <-- new_node
     }
-    
-     public void printList() {
-        Node current = head;  
-        
+
+    public int deleteFirst() {
+        int temp = front.data;
+        if (front.next == null) {  // If only one element
+            rear = null;
+        }
+        front = front.next;
+        return temp;
+    }
+
+    public int deleteLast() {
+        int temp = rear.data;
+        Node current = front;
+
+        while (current.next != rear) {  // Traverse to second last node
+            current = current.next;
+        }
+        current.next = null;
+        rear = current;
+        return temp;
+    }
+
+    void displayList() {
+        System.out.println("==== Displaying Elements of DEQ ====");
+        Node current = front;
         while (current != null) {
-            System.out.println(current.data + " ");  
-            current = current.address; 
+            System.out.println(current.data);
+            current = current.next;
         }
- 
-    }              
-    public static void main(String[] args) {            
-//        Node node1 = new Node(1);
-//        Node node2= new Node(2);
-//        Node node3 = new Node(3);
-//        Node node4 = new Node(4);
-//        Node node5 = new Node(5);
-        
-//        node1.address = node2;
-//        node2.address = node3;
-//        node3.address = node4;
-//        node4.address = node5;
-//        node5.address = null;
-       
-        LinkedList list = new LinkedList();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        
-        list.printList(); 
-        System.out.println("The size of the list is: " +list.getSize());
-//       System.out.println(node1.data);
-//       System.out.println(node1.address.data); // node1.address.data = node2.data
-//       System.out.println(node1.address.address.data); // node1.address.address.data = node2.address.data = node3.data
-//       System.out.println(node1.address.address.address.data); //node1.address.address.address.data = node2.address.address.data = node3.address.data = node4.data
-//       System.out.println(node1.address.address.address.address.data); //node1.address.address.address.address.data = node2.address.address.address.data = node3.address.address.data = node4.address.data = node5.data
+    }
+}
+
+class DoubleEndedQueue {
+    LinkedList llist;  // Updated to use LinkedList
+
+    public DoubleEndedQueue() {
+        llist = new LinkedList();  // Updated to use LinkedList
+    }
+
+    public boolean isEmpty() {
+        return llist.isEmpty();
+    }
+
+    public void enqueueAtRear(int new_data) {
+        llist.insertLast(new_data);
+    }
+
+    public void enqueueAtFront(int new_data) {
+        llist.insertFirst(new_data);
+    }
+
+    public int dequeueFromFront() {
+        return llist.deleteFirst();
+    }
+
+    public int dequeueFromRear() {
+        return llist.deleteLast();
+    }
+
+    void printQueue() {
+        llist.displayList();
+    }
+}
+
+public class Linkedlist { // Main class should remain Linkedlist
+    public static void main(String[] args) {
+        DoubleEndedQueue deq = new DoubleEndedQueue();
+        deq.enqueueAtRear(12);
+        deq.enqueueAtRear(13);
+        deq.enqueueAtRear(18);
+        deq.printQueue();
+        deq.enqueueAtFront(30);
+        deq.printQueue();
+        int element1 = deq.dequeueFromFront();
+        System.out.println("Dequeued element (from front) is " + element1);
+        deq.printQueue();
+        int element2 = deq.dequeueFromRear();
+        System.out.println("Dequeued element (from rear) is " + element2);
+        deq.printQueue();
     }
 }
